50c0dcea6ba5e1aaa8b9f11122bddada24fca833
Simon H <github@pointbeing.net>
2025-05-23 15:15:50 +0100

    Add solution for 2025-05-11

--- safe_cracker.pl
+++ safe_cracker.pl
@@ -1,6 +1,8 @@
 :- module(safe_cracker, [
     common_constraints/1,
+    divides_by/2,
     is_prime/2,
+    occurrenceof/3,
     xor/2
 ]).
 :- use_module(library(clpfd)).
@@ -18,3 +20,23 @@ xor(X, Y) :-
 
 is_prime(N, 1) :- N in {2, 3, 5, 7}.
 is_prime(N, 0) :- N in {1, 4, 6, 8, 9}.
+
+
+divides_by(X, Y) :-
+    X mod Y =:= 0.
+
+
+occurrenceof([], _, 0). %empty list, count of anything is 0. Base case.
+
+% The first item in the list is the same as what you want to count so
+% add 1 to the recursive count.
+occurrenceof([H|T], H, NewCount) :-
+    occurrenceof(T, H, OldCount),
+    NewCount is OldCount + 1.
+
+% The first item in the list is different so keep old count
+occurrenceof([H|T], H2, Count) :-
+    dif(H, H2),
+    occurrenceof(T, H2, Count).
+
+
---
+++ solutions/safe20250511.pl
@@ -0,0 +1,29 @@
+:- use_module('../safe_cracker').
+
+solution_20250511(A, B, C, D) :-
+    Vs = [A, B, C, D],
+    Vs ins 1..9,
+    all_distinct(Vs),
+
+    % 1. Exactly one of the digits is 1
+    occurrenceof(Vs, 1, 1),
+
+    % 2. The first and third total 13
+    A + C #= 13,
+
+    % 3. The third and fourth differ by 2
+    abs(C - D) #= 2,
+
+    % 4. The first is greater than the fourth
+    A #> D,
+
+    % 5. Exactly two digits are divisible by 3
+    maplist(divides_by_3, Vs, Threes),
+    sum(Threes, #=, 2),
+
+    label(Vs).
+
+
+divides_by_3(N, B) :- (N mod 3 #= 0) #<==> B.
+
+% findall([A, B, C, D], solution_20250511(A, B, C, D), Solutions), length(Solutions, Count).
