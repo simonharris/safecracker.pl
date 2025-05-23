77088c6bac1ca4c8636aa276c4c88a2a1425202c
Simon H <github@pointbeing.net>
2025-05-23 09:43:23 +0100

    Add comments for constraints

--- solutions/safe20250413.pl
+++ solutions/safe20250413.pl
@@ -5,9 +5,19 @@ solution_20250413(A, B, C, D) :-
     Vs = [A, B, C, D],
     common_constraints(Vs),
 
+    % 1. The fourth is 3 more than the first
     D #= A + 3,
+
+    % 2. Either the second or the third is odd, but not both
     xor(B mod 2 #= 1, C mod 2 #= 1),
+
+    % 3. The fourth digit is less than 7
     D #< 7,
+
+    % 4. The second is greater than the first
     B #> A,
+
+    % 5. The fourth is greater than the sum of the second and fourth
     D #> (B + C),
+
     label(Vs).
