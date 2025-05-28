95a7a6e27c44c8d0e0fb938b90808eb7e360fe7f
Simon H <github@pointbeing.net>
2025-05-28 02:09:43 +0100

    Solution for the graduation teaser

---
+++ solutions/tease1904_graduation.pl
@@ -0,0 +1,33 @@
+:- use_module(library(clpfd)).
+
+
+% facts -----------------------------------------------------------------------
+
+
+% you don't need this, except as documentation
+venue_id(manorhall, 1).
+venue_id(cathedral, 2).
+venue_id(garden, 4).
+venue_id(courtyard, 8).
+
+
+% rules -----------------------------------------------------------------------
+
+
+solution(MathsVenue, HistoryVenue, LawVenue, MusicVenue) :-
+    Venues = [MathsVenue, HistoryVenue, LawVenue, MusicVenue],
+    Venues ins 1 \/ 2 \/ 4 \/ 8,
+    all_distinct(Venues),
+
+    LawVenue #\= 1,
+    LawVenue #\= 2,
+
+    (MusicVenue #= 1; MusicVenue #= 2),
+    (HistoryVenue #= 1; HistoryVenue #= 8),
+
+    HistoryVenue #= 8 #<==> MathsVenue #= 4,
+
+    label(Venues).
+
+
+% findall([MathsVenue, HistoryVenue, LawVenue, MusicVenue], solution(MathsVenue, HistoryVenue, LawVenue, MusicVenue), Solutions), length(Solutions, Count).
