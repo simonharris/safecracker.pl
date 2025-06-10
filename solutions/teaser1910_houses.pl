c2b487bfa896c8810c99c379671be9bfe3408da7
Simon H <github@pointbeing.net>
2025-06-10 19:35:33 +0100

    Add solution for teaset 1920_houses

---
+++ solutions/teaser1910_houses.pl
@@ -0,0 +1,65 @@
+
+% Crypticville neighbours are discussing where they live. "Fay lives next to
+% May but not in the red house; Ray lives next to the grey house and No 8 is
+% not the blue house," said Jay. "No 4 is between Jay and the white house, May
+% does not live next to No 2 and Jay does not live at No 6 or the blue house,"
+% said Fay. Where does each person live?
+
+
+solution1910(Two, Four, Six, Eight) :-
+    Street = [Two, Four, Six, Eight],
+
+    Two = TwoN-TwoC,
+    Four = FourN-FourC,
+    Six = SixN-SixC,
+    Eight = EightN-EightC,
+
+    Names = [fay, jay, may, ray],
+    Colors = [blue, grey, red, white],
+
+    permutation(Names, [TwoN, FourN, SixN, EightN]),
+    permutation(Colors, [TwoC, FourC, SixC, EightC]),
+
+    % Fay lives next to May...
+    next_to(fay, may, Street),
+
+    % ...but not in the red house
+    not_in_colour(fay, red, Street),
+
+    % Ray lives next to the grey house
+    next_to_colour(ray, grey, Street),
+
+    % No 8 is not the blue house
+    EightC \= blue,
+
+    % No 4 is between Jay and the white house
+    not_in_colour(jay, white, Street),
+    (TwoN = jay ; SixN = jay),
+    (TwoC = white ; SixC = white),
+
+    % May does not live next to No 2
+    FourN \= may,
+
+    % Jay does not live at No 6...
+    SixN \= jay,
+
+    % ...or the blue house
+    not_in_colour(jay, blue, Street).
+
+
+next_to(Name1, Name2, List) :-
+    append(_, [N1-_, N2-_|_], List),
+    (N1 = Name1, N2 = Name2 ; N1 = Name2, N2 = Name1).
+
+
+next_to_colour(Name, Colour, [H|T]) :-
+    next_to_colour_helper([H|T], Name, Colour).
+
+next_to_colour_helper([Name-_, _-Colour|_], Name, Colour).
+next_to_colour_helper([_-Colour, Name-_|_], Name, Colour).
+next_to_colour_helper([_|T], Name, Colour) :-
+    next_to_colour_helper(T, Name, Colour).
+
+
+not_in_colour(Person, Color, Street) :-
+    \+ member(Person-Color, Street).
--- tests/test_solutions_teaser.pl
+++ tests/test_solutions_teaser.pl
@@ -1,9 +1,9 @@
 :- begin_tests(brain_teaser_solutions).
 :- consult('../solutions/teaser1904_graduation').
 :- consult('../solutions/teaser1905_dice').
+:- consult('../solutions/teaser1910_houses').
 
 % These are largely pointless, but think of it as checking the solutions page
-
 
 test(solution_1904_graduation) :-
     once(solution_1904(MathsVenue, HistoryVenue, LawVenue, MusicVenue)),
@@ -12,3 +12,7 @@ test(solution_1904_graduation) :-
 test(solution_1905_graduation) :-
     once(solution1905(Jackie1, Jackie2, Ethel1, Ethel2, Len1, Len2)),
     assertion((Jackie1 = 3, Jackie2 = 5, Ethel1 = 6, Ethel2 = 2, Len1 = 1, Len2 = 4)).
+
+test(solution_1910_houses) :-
+    once(solution1910(Two, Four, Six, Eight)),
+    assertion((Two = jay-grey, Four = ray-blue, Six = fay-white, Eight = may-red)).
