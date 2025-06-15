5d8e2720dec803005cefa12fb40b97320d013c1f
Simon H <github@pointbeing.net>
2025-06-15 22:04:48 +0100

    First tiny steps towards a DCG for parsing constraints

---
+++ parser/grammar.pl
@@ -0,0 +1,31 @@
+
+clue --> digit, i, op, num.
+
+digit --> det, ord.
+digit --> det, ord, d.
+
+det --> ['the'].
+
+ord --> ['first'].
+ord --> ['second'].
+ord --> ['third'].
+ord --> ['fourth'].
+
+d --> ['digit'].
+d --> ['number'].
+
+i --> ['is'].
+
+op --> ['less',  'than'].
+op --> ['greater',  'than'].
+op --> ['equal',  'to'].
+
+num --> ['1'].
+num --> ['2'].
+num --> ['3'].
+num --> ['4'].
+num --> ['5'].
+num --> ['6'].
+num --> ['7'].
+num --> ['8'].
+num --> ['9'].
