cd7494d432dd23e058102d522aa7ba460f0d791f
Simon H <github@pointbeing.net>
2025-06-15 15:32:20 +0100

    Add template for Safe Cracker solutions

---
+++ solutions/safe_template.pl
@@ -0,0 +1,23 @@
+:- use_module('../safe_cracker').
+
+
+solution_20250615(A, B, C, D) :-
+    Vs = [A, B, C, D],
+    common_constraints(Vs),
+
+    % 1.
+
+
+    % 2.
+
+
+    % 3.
+
+
+    % 4.
+
+
+    % 5.
+
+
+    label(Vs).
