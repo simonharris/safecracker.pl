3d3419707d9fc18decbe486e7346ea678d9f4609
Simon H <github@pointbeing.net>
2025-06-18 10:24:04 +0100

    Tighten up the regex

--- ocr/test.py
+++ ocr/test.py
@@ -5,7 +5,7 @@ from PIL import Image
 import pytesseract
 
 
-REGEX = r'^[1-5]\s([A-Za-z-0-9 ]+)[\.\n]'
+REGEX = r'^[1-5] ([A-Za-z-0-9 ]+)[\.\n]'
 
 imgfile = sys.argv[1]
 content = pytesseract.image_to_string(Image.open(imgfile))
