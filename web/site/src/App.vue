1dcedcafaad3bb475441cc6fcb5f6833b9cdcaa5
Simon H <github@pointbeing.net>
2025-06-22 18:37:55 +0100

    Improve wonky nav

--- web/site/src/App.vue
+++ web/site/src/App.vue
@@ -13,7 +13,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
           <router-link class="nav-link" to="/">Upload</router-link>
         </li>
