10685bc07dff95241736bdaa96fc6b65fecceda4
Simon H <github@pointbeing.net>
2025-07-03 20:56:23 +0100

    Move away from deprecated apply/2

--- parser/solutions/safe20250615.pl
+++ parser/solutions/safe20250615.pl
@@ -7,23 +7,23 @@ solution_20250615(A, B, C, D) :-
 
     % 1. The first digit is greater than five
     % A #> 5,
-    apply('The first digit is greater than five', Vs),
+    apply_clue('The first digit is greater than five', Vs),
 
     % 2. The third is two less than the fourth
     % C #= D - 2,
-    apply('The third is two less than the fourth', Vs),
+    apply_clue('The third is two less than the fourth', Vs),
 
     % 3. The fourth is less than the second
     %D #< B,
-    apply('The fourth is less than the second', Vs),
+    apply_clue('The fourth is less than the second', Vs),
 
     % 4. Only one digit is odd
     % include(is_odd, Vs, Odds),
     % length(Odds, 1),
-    apply('Only one digit is odd', Vs),
+    apply_clue('Only one digit is odd', Vs),
 
     % 5. The first and fourth differ by one
     % abs(A - D) #= 1,
-    apply('The first and fourth differ by one', Vs),
+    apply_clue('The first and fourth differ by one', Vs),
 
     label(Vs).
