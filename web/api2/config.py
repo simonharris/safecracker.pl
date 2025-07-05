bb97cdad753dac457e42e2ebec6fbb7b3d929f3d
Simon H <github@pointbeing.net>
2025-07-05 17:58:12 +0100

    File upload edpoint works on FastAPI

---
+++ web/api2/config.py
@@ -0,0 +1,8 @@
+
+SOLVER_FILE = 'parser/solver.pl'
+SOLVER_PRED = 'solution(A, B, C, D), halt'
+COUNTER_PRED = 'solution_count(A, B, C, D, Count), writeln(Count), halt.'
+
+EXAMPLE_DIR = 'static/examples/'
+UPLOAD_DIR = 'static/uploads/'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'} # TODO: explore others
