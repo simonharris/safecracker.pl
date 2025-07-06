971a4e8171992d5e011e5b0ccef8f97bd6ffa823
Simon H <github@pointbeing.net>
2025-07-06 18:37:18 +0100

    Remove apply/2 calls

--- parser/solutions/safe20250504.pl
+++ parser/solutions/safe20250504.pl
@@ -1,4 +1,5 @@
 :- use_module('../safe_cracker').
+:- use_module('../parser').
 
 
 solution_20250504(A, B, C, D) :-
@@ -7,24 +8,24 @@ solution_20250504(A, B, C, D) :-
 
     % 1. The third digit is less than five
     % C #< 5,
-    apply('The third digit is less than five', Vs),
+    apply_clue('The third digit is less than five', Vs),
 
     % 2. The second is twice the fourth
     %B #= 2 * D,
-    apply('The second is twice the fourth', Vs),
+    apply_clue('The second is twice the fourth', Vs),
 
     % 3. Exactly two digits are prime
     % maplist(is_prime, Vs, PrimeDigits),
     % sum(PrimeDigits, #=, 2),
-    apply('Exactly two digits are prime', Vs),
+    apply_clue('Exactly two digits are prime', Vs),
 
     % 4. The second exceeds the first by more than two
     % B #> A + 2,
-    apply('The second exceeds the first by more than two', Vs),
+    apply_clue('The second exceeds the first by more than two', Vs),
 
     % 5. The first and third differ by three
     % abs(A - C) #= 3,
-    apply('The first and third differ by three', Vs),
+    apply_clue('The first and third differ by three', Vs),
 
     label(Vs).
 
