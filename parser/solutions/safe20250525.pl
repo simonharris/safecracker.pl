b8c1d57561f38fc3f8bb22121e0d6df545f24b28
Simon H <github@pointbeing.net>
2025-07-06 18:37:18 +0100

    Remove apply/2 calls

--- parser/solutions/safe20250525.pl
+++ parser/solutions/safe20250525.pl
@@ -1,4 +1,5 @@
 :- use_module('../safe_cracker').
+:- use_module('../parser').
 
 
 solution_20250525(A, B, C, D) :-
@@ -7,22 +8,22 @@ solution_20250525(A, B, C, D) :-
 
     % 1. The fourth digit is odd
     % is_odd(D),
-    apply('The fourth digit is odd', Vs),
+    apply_clue('The fourth digit is odd', Vs),
 
     % 2. The first and second total the third
     % (A + B) #= C,
-    apply('The first and second total the third', Vs),
+    apply_clue('The first and second total the third', Vs),
 
     % 3. Exactly one digit is square
     % include(is_square, Vs, [_]),
-    apply('Exactly one digit is square', Vs),
+    apply_clue('Exactly one digit is square', Vs),
 
     % 4. The fourth is two more than the first
     % D #= A + 2,
-    apply('The fourth is two more than the first', Vs),
+    apply_clue('The fourth is two more than the first', Vs),
 
     % 5. The first and third differ by one
     %abs(A - C) #= 1,
-    apply('The first and third differ by one', Vs),
+    apply_clue('The first and third differ by one', Vs),
 
     label(Vs).
