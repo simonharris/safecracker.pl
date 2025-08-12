5143a52cce69d6e514267ab77675be81cfd3fcff
Simon H <github@pointbeing.net>
2025-08-12 13:03:04 +0100

    Move some tests to plunit_assert

--- parser/tests/test_safe_cracker.pl
+++ parser/tests/test_safe_cracker.pl
@@ -1,49 +1,52 @@
+:- use_module(library(plunit_assert)).
 :- use_module('../safe_cracker').
 
 
 :- begin_tests(safe_cracker).
 
 test(xor) :-
-    assertion(xor(1, 0)),
-    assertion(xor(0, 1)),
-    assertion(\+ xor(1, 1)),
-    assertion(\+ xor(0, 0)).
+    assert_true(xor(1, 0)),
+    assert_true(xor(0, 1)),
+    assert_false(xor(1, 1)),
+    assert_false(xor(0, 0)).
 
 test(is_prime) :-
-    assertion(is_prime(17)),
-    assertion(\+ is_prime(6)),
-    assertion(is_prime(2)),
-    assertion(is_prime(2 + 1)),
-    assertion(\+ is_prime(1)),
+    assert_true(is_prime(17)),
+    assert_false(is_prime(6)),
+    assert_true(is_prime(2)),
+    assert_true(is_prime(2 + 1)),
+    assert_false(is_prime(1)),
+    assert_exception(is_prime(hello)),
     !.
 
 test(is_square) :-
-    assertion(is_square(1)),
-    assertion(\+ is_square(7)),
-    assertion(is_square(4)),
-    assertion(\+is_square(13)),
-    assertion(is_square(16)).
+    assert_true(is_square(1)),
+    assert_false(is_square(7)),
+    assert_true(is_square(4)),
+    assert_false(is_square(13)),
+    assert_true(is_square(16)).
 
 test(is_odd) :-
-    assertion(\+ is_odd(0)),
-    assertion(\+ is_odd(4)),
-    assertion(is_odd(7)).
+    assert_false(is_odd(0)),
+    assert_false(is_odd(4)),
+    assert_true(is_odd(7)).
 
 test(is_even) :-
-    assertion(is_even(0)),
-    assertion(\+ is_even(77)),
-    assertion(is_even(44)).
+    assert_true(is_even(0)),
+    assert_false(is_even(77)),
+    assert_true(is_even(44)).
 
 test(divides_by) :-
-    assertion(\+ divides_by(15, 2)),
-    assertion(divides_by(15, 5)),
-    assertion(divides_by(4, 2)).
+    assert_false(divides_by(15, 2)),
+    assert_true(divides_by(15, 5)),
+    assert_true(divides_by(4, 2)).
 
 test(occurrenceof) :-
     assertion(occurrenceof([], 1, 0)),
     assertion(occurrenceof([2, 3, 4, 5], 1, 0)),
     assertion(occurrenceof([1, 2, 3, 4], 1, 1)),
     assertion(occurrenceof([1, 2, 3, 4, 1, 1], 1, 3)).
+
 
 :- end_tests(safe_cracker).
 
