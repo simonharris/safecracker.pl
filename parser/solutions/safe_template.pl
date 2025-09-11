3eb353192aebf09316418cb028cd910afe3ea7bc
Simon H <github@pointbeing.net>
2025-09-11 22:55:47 +0100

    Make template a little more helpful

--- parser/solutions/safe_template.pl
+++ parser/solutions/safe_template.pl
@@ -6,19 +6,10 @@ solution_20250615(A, B, C, D) :-
     Vs = [A, B, C, D],
     common_constraints(Vs),
 
-    % 1.
-
-
-    % 2.
-
-
-    % 3.
-
-
-    % 4.
-
-
-    % 5.
-
+    % apply_clue('', Vs),
+    % apply_clue('', Vs),
+    % apply_clue('', Vs),
+    % apply_clue('', Vs),
+    % apply_clue('', Vs),
 
     label(Vs).
